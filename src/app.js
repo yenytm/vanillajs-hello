36ba37b6bc0881568588bcdd1a1680c3ab9a99d1
yenytm <143008413+yenytm@users.noreply.github.com>
2023-10-07 00:11:50 +0000

    all finito

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My sister", "Her unicorn"];
+  let action = ["ate", "flew", "stole"];
+  let what = ["my homework", "my phone", "my keys"];
+  let when = ["before class", "this morning", "during lunch"];
+
+  function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  let excuse =
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when);
+
   console.log("Hello Rigo from the console!");
+  console.log(excuse);
+
+  const elementExcuse = document.getElementsByTagName("p");
+  elementExcuse[0].innerText = excuse;
 };
