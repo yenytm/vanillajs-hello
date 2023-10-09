f47298899d540161d207c3b06013a6f71b4ea5ea
yenytm <143008413+yenytm@users.noreply.github.com>
2023-10-09 05:12:33 +0000

    fix concatenation

--- src/app.js
+++ src/app.js
@@ -7,22 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let subject = ["The dog", "My sister", "Her unicorn"];
-  let verb = ["ate", "flew", "stole"];
-  let object = ["my homework", "my phone", "my keys"];
-  let time = ["before class", "this morning", "during lunch"];
+  let subject = ["The dog", "My sister", "Her unicorn", "My cat"];
+  let verb = ["ate", "flew", "stole", "left with"];
+  let object = ["my homework", "my phone", "my keys", "my car", "my laptop"];
+  let time = [
+    "before class.",
+    "this morning.",
+    "during lunch.",
+    "before class."
+  ];
 
   function randExcuse(str) {
     return str[Math.floor(Math.random() * str.length)];
   }
-  let excuseGen =
-    randExcuse(subject) +
-    " " +
-    randExcuse(verb) +
-    " " +
-    randExcuse(object) +
-    " " +
-    randExcuse(time);
+  let excuseGen = randExcuse(subject).concat(
+    " ",
+    randExcuse(verb),
+    " ",
+    randExcuse(object),
+    " ",
+    randExcuse(time)
+  );
 
   console.log("Hello Rigo from the console!");
   console.log(excuseGen);
